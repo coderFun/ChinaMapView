5e2e42e9770ec63a6020e5a524774eb7bf49345c
coderfun <colderfun@sina.com>
2016-03-30 23:24:27 +0800

    简单的添加了双指移动的事件

--- library/src/main/java/com/coderfun/chinamapview/library/ChinaMapView.java
+++ library/src/main/java/com/coderfun/chinamapview/library/ChinaMapView.java
@@ -8,13 +8,14 @@ import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.Region;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
 
 /**
- *
+ * a view to show a china map
  */
 public class ChinaMapView extends View {
     public static final String TAG = "ChinaMapView";
@@ -30,6 +31,10 @@ public class ChinaMapView extends View {
     Region region = new Region();
 
     boolean needInvalidate = true;
+
+    float translateX;
+    float translateY;
+    float scaleRatio;
 
     public ChinaMapView(Context context) {
         super(context);
@@ -79,51 +84,86 @@ public class ChinaMapView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (needInvalidate){
-            float xRatio = getWidth()/(float)rectMap.width();
-            float yRatio = getHeight()/(float)rectMap.height();
-            if (xRatio>yRatio){
-                matrix.setScale(yRatio, yRatio);
-            }else{
-                matrix.setScale(xRatio, xRatio);
-            }
-            for (int i=0;i<regions.size();i++){
-                paths.get(i).transform(matrix,distPaths.get(i));
-                regions.get(i).setPath(distPaths.get(i),region);
+        if (needInvalidate) {
+            matrix.reset();
+            float xRatio = getMeasuredWidth() / (float) rectMap.width();
+            float yRatio = getMeasuredHeight() / (float) rectMap.height();
+            scaleRatio = xRatio > yRatio ? yRatio : xRatio;
+            matrix.setScale(scaleRatio, scaleRatio);
+            matrix.postTranslate(translateX, translateY);
+            //matrix.setTranslate(translateX, translateY);
+            for (int i = 0; i < regions.size(); i++) {
+                paths.get(i).transform(matrix, distPaths.get(i));
+                regions.get(i).setPath(distPaths.get(i), region);
             }
             needInvalidate = false;
         }
 
         for (int i = 0; i < paths.size(); i++) {
-            canvas.drawPath(distPaths.get(i),paint);
+            canvas.drawPath(distPaths.get(i), paint);
         }
         if (markedPath != null) {
             canvas.drawPath(markedPath, paintMarked);
         }
     }
 
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+
+    float tempX;
+    float tempY;
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_MOVE:
-                int x = (int) event.getX();
-                int y = (int) event.getY();
-                markedPath = null;
-                for (int i = 0; i < regions.size(); i++) {
-                    if (regions.get(i).contains(x, y)) {
-                        markedPath = distPaths.get(i);
-                        break;
+        //Log.e(TAG, "onTouchEvent: " + event.getPointerCount());
+        if (event.getPointerCount() == 2) {
+            Log.e(TAG, "onTouchEvent: " + event.getActionMasked());
+            markedPath = null;
+            switch (event.getActionMasked()) {
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    x1 = (int) event.getX(0);
+                    x2 = (int) event.getX(1);
+                    y1 = (int) event.getY(0);
+                    y2 = (int) event.getX(1);
+                    tempX = translateX;
+                    tempY = translateY;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    translateX = tempX + event.getX(0) - x1;
+                    translateY = tempY + event.getY(0) - y1;
+                    needInvalidate = true;
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    break;
+
+            }
+        } else {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_MOVE:
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
+                    markedPath = null;
+                    for (int i = 0; i < regions.size(); i++) {
+                        if (regions.get(i).contains(x, y)) {
+                            markedPath = distPaths.get(i);
+                            break;
+                        }
                     }
-                }
-                invalidate();
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                markedPath = null;
-                invalidate();
-                break;
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    markedPath = null;
+                    invalidate();
+                    break;
+            }
         }
+
         return super.onTouchEvent(event);
     }
 }
