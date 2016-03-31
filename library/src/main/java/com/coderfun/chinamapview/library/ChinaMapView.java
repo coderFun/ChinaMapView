9633831181f5cf862c6baa10c9d8c0f6708092da
coderfun <colderfun@sina.com>
2016-03-31 22:28:22 +0800

    简单添加了缩放手势，不过有明显bug。。。

--- library/src/main/java/com/coderfun/chinamapview/library/ChinaMapView.java
+++ library/src/main/java/com/coderfun/chinamapview/library/ChinaMapView.java
@@ -6,6 +6,7 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Region;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -23,14 +24,14 @@ public class ChinaMapView extends View {
     Paint paintMarked;
 
     Matrix matrix = new Matrix();
-    Rect rectMap = new Rect();
+    RectF rectMap = new RectF();
+    RectF rectMapDist = new RectF();
     ArrayList<Region> regions = new ArrayList<>();
     ArrayList<Path> paths = new ArrayList<>();
     ArrayList<Path> distPaths = new ArrayList<>();
     private Path markedPath;
     Region region = new Region();
 
-    boolean needInvalidate = true;
 
     float translateX;
     float translateY;
@@ -72,32 +73,37 @@ public class ChinaMapView extends View {
             paths.add(SVGParser.parsePath(ChinaMap.svgProvinces[i]));
             regions.add(new Region());
         }
-
+        RectF rectF = new RectF();
         for (int i = 0; i < regions.size(); i++) {
             regions.get(i).setPath(paths.get(i), region);
-            rectMap.union(regions.get(i).getBounds());
+            rectF.set(regions.get(i).getBounds());
+            rectMap.union(rectF);
             distPaths.add(new Path());
         }
+        rectMapDist.set(rectMap);
+    }
 
+    @Override
+    protected void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Log.e(TAG, "onSizeChanged: ");
+
+        float xRatio = getMeasuredWidth() /  rectMap.width();
+        float yRatio = getMeasuredHeight() / rectMap.height();
+        scaleRatio = xRatio > yRatio ? yRatio : xRatio;
+        resizeMap(true);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (needInvalidate) {
-            matrix.reset();
-            float xRatio = getMeasuredWidth() / (float) rectMap.width();
-            float yRatio = getMeasuredHeight() / (float) rectMap.height();
-            scaleRatio = xRatio > yRatio ? yRatio : xRatio;
-            matrix.setScale(scaleRatio, scaleRatio);
-            matrix.postTranslate(translateX, translateY);
-            //matrix.setTranslate(translateX, translateY);
-            for (int i = 0; i < regions.size(); i++) {
-                paths.get(i).transform(matrix, distPaths.get(i));
-                regions.get(i).setPath(distPaths.get(i), region);
-            }
-            needInvalidate = false;
-        }
+
 
         for (int i = 0; i < paths.size(); i++) {
             canvas.drawPath(distPaths.get(i), paint);
@@ -107,19 +113,34 @@ public class ChinaMapView extends View {
         }
     }
 
+    private void resizeMap(boolean shouldRegion) {
+        matrix.reset();
+        matrix.setScale(scaleRatio, scaleRatio, rectMapDist.centerX(), rectMapDist.centerY());
+        matrix.postTranslate(translateX, translateY);
+        for (int i = 0; i < regions.size(); i++) {
+            paths.get(i).transform(matrix, distPaths.get(i));
+        }
+        if(shouldRegion){
+            for (int i = 0; i < regions.size(); i++) {
+                regions.get(i).setPath(distPaths.get(i), region);
+            }
+        }
+
+    }
+
     int x1;
     int x2;
     int y1;
     int y2;
-
+    double distance;
     float tempX;
     float tempY;
-
+    float tempScale;
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         //Log.e(TAG, "onTouchEvent: " + event.getPointerCount());
         if (event.getPointerCount() == 2) {
-            Log.e(TAG, "onTouchEvent: " + event.getActionMasked());
+            //Log.e(TAG, "onTouchEvent: " + event.getActionMasked());
             markedPath = null;
             switch (event.getActionMasked()) {
                 case MotionEvent.ACTION_POINTER_DOWN:
@@ -127,17 +148,24 @@ public class ChinaMapView extends View {
                     x2 = (int) event.getX(1);
                     y1 = (int) event.getY(0);
                     y2 = (int) event.getX(1);
+                    distance =  Math.sqrt(Math.pow((event.getX(1)-event.getX(0)),2)+Math.pow((event.getX(1)-event.getY(0)),2));
                     tempX = translateX;
                     tempY = translateY;
+                    tempScale = scaleRatio;
                     break;
                 case MotionEvent.ACTION_MOVE:
                     translateX = tempX + event.getX(0) - x1;
                     translateY = tempY + event.getY(0) - y1;
-                    needInvalidate = true;
+                    double distance2 = Math.sqrt(Math.pow((event.getX(1)-event.getX(0)),2)+Math.pow((event.getX(1)-event.getY(0)),2));
+                    if(Math.abs(distance2-distance)>20){
+                        scaleRatio = tempScale * (float) ((distance2-20) /distance);
+                    }
+                    resizeMap(false);
                     invalidate();
                     break;
                 case MotionEvent.ACTION_POINTER_UP:
                 case MotionEvent.ACTION_CANCEL:
+                    resizeMap(true);
                     break;
 
             }
